4b18be8af9575e57a9f60a5ea18b970b564b95fe
CodeExamplesTutorials <166758642+CodeExamplesTutorials@users.noreply.github.com>
2024-04-20 15:30:29 +0200

    Create 04_Comments.c

---
+++ Basics/04_Comments.c
@@ -0,0 +1,26 @@
+/* (c) advancedprogramming.org / info@advancedprogramming.org */
+
+#include <stdio.h>
+#include <stdint.h>
+
+
+int main(void)
+{
+	/* Comment */
+
+	/* Multi Line Comment
+	 *
+	 *
+	 */
+
+
+
+//	Test
+
+//	Multi line
+//	Multi line
+
+
+
+  return 0;
+}
