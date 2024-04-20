8cf133b20fa229596c43816bac03cb782b6514ac
CodeExamplesTutorials <166758642+CodeExamplesTutorials@users.noreply.github.com>
2024-04-20 16:12:08 +0200

    Create 06_Operators_Comparision.c

---
+++ Basics/06_Operators_Comparision.c
@@ -0,0 +1,44 @@
+/* (c) advancedprogramming.org / info@advancedprogramming.org */
+
+#include <stdio.h>
+#include <stdint.h>
+
+
+int main(void)
+{
+	uint8_t bValue = 10;
+	setvbuf(stdout, NULL, _IONBF, 0);
+
+	if(10 > bValue)
+	{
+		printf("Smaller 10\n");
+	}
+
+	if(10 >= bValue)
+	{
+		printf("Smaller or equal 10\n");
+	}
+
+	if(10 == bValue)
+	{
+		printf("Identical 10\n");
+	}
+
+	if(10 != bValue)
+	{
+		printf("Not 10\n");
+	}
+
+	if(10 < bValue)
+	{
+		printf("\n");
+	}
+
+	if(10 <= bValue)
+	{
+		printf("Smaller or equal 10\n");
+	}
+
+
+  return 0;
+}
