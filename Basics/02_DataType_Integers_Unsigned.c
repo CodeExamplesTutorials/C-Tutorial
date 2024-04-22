9ef67e26ea9be67c7ca6f657339d57e882909c0c
Denis <Denis@LAPTOP-5VDTKDKA.localdomain>
2024-04-22 21:10:37 +0200

    patch

--- Basics/02_DataType_Integers_Unsigned.c
+++ Basics/02_DataType_Integers_Unsigned.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <stdint.h>
 
-
 int main(void)
 {
   uint8_t  ubValue = 0;
@@ -11,13 +10,10 @@ int main(void)
   uint32_t ulValue = 0;
   uint64_t ullValue = 0;
 
-
-  /* unsigned */
   ubValue  = 0xFF;
   usValue  = 0xFFFF;
   ulValue  = 0xFFFFFFFF;
   ullValue = 0xFFFFFFFFFFFFFFFF;
-
 
  (void)ubValue;
  (void)usValue;
