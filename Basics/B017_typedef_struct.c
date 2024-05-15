5339198de07363d8135784ffb4646bfdd5d61aa4
janssen <janssen@janssen-System-Product-Name>
2024-05-15 21:56:09 +0200

    update

--- Basics/B017_typedef_struct.c
+++ Basics/B017_typedef_struct.c
@@ -3,21 +3,17 @@
 #include <stdint.h>
 #include <inttypes.h>
 
-//typedef struct LOCAL_RSC_Ttag LOCAL_RSC_T;
-//
-//struct LOCAL_RSC_Ttag
-//{
-//  uint32_t u32Value1;
-//  uint32_t u32Value2;
-//  uint32_t u32Result;
-////  LOCAL_RSC_T* ptRsc;
-//};
-
-typedef struct LOCAL_RSC_Ttag
+typedef struct LOCAL_VAL_Ttag
 {
   uint32_t u32Value1;
   uint32_t u32Value2;
-  uint32_t u32Result;
+}LOCAL_VAL_T;
+
+
+typedef struct LOCAL_RSC_Ttag
+{
+  LOCAL_VAL_T tValues;
+  uint32_t    u32Result;
 }LOCAL_RSC_T;
 
 
@@ -26,10 +22,10 @@ int main(void)
   LOCAL_RSC_T tRsc = {0};
   setvbuf(stdout, NULL, _IONBF, 0);
 
-  tRsc.u32Value1 = 2;
-  tRsc.u32Value2 = 4;
+  tRsc.tValues.u32Value1 = 2;
+  tRsc.tValues.u32Value2 = 4;
 
-  tRsc.u32Result = tRsc.u32Value1  + tRsc.u32Value2;
+  tRsc.u32Result = tRsc.tValues.u32Value1  + tRsc.tValues.u32Value2;
 
 
   printf("Value: %"PRIu8"\n", tRsc.u32Result);
